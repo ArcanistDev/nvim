d4d0ce34e7a6737e5d11121a9c542ed9168c7c7a
ShadeyMage <shade@arcanist.college>
2025-01-19 16:02:12 -0600

    added more files

---
+++ lua/mage/plugins/miniextras.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    require("mini.basics").setup({}),
+    require("mini.cursorword").setup({}),
+    require("mini.surround").setup({}),
+    require("mini.splitjoin").setup({}),
+    require("mini.clue").setup({})
+  }
+}
