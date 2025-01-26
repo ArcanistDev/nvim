ccb4007199ac0485f202dfeaa3e61f22ec40d95e
ShadeyMage <shade@arcanist.college>
2025-01-25 21:38:45 -0600

    fixed more config

--- lua/mage/plugins/miniextras.lua
+++ lua/mage/plugins/miniextras.lua
@@ -1,9 +1,12 @@
 return {
   {
+    require("mini.icons").setup({}),
     require("mini.basics").setup({}),
     require("mini.cursorword").setup({}),
     require("mini.surround").setup({}),
     require("mini.splitjoin").setup({}),
-    require("mini.clue").setup({})
+    require("mini.clue").setup({}),
+    require("mini.comment").setup({}),
+    require("mini.completion").setup({})
   }
 }
