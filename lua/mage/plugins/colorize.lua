6da5431f1a2fd2df15351fd10ddff091a0039ea5
ArcanistDev <shade@arcanist.college>
2025-02-10 21:58:40 -0600

    Change colorize rules. Again

--- lua/mage/plugins/colorize.lua
+++ lua/mage/plugins/colorize.lua
@@ -6,7 +6,8 @@ return {
     require("colorizer").setup()
     require("colorizer").setup {
       '*',
-      conf = { RRGGBBAA = true, rgb_fn = true, hsl_fn = true },
+      conf = { RRGGBBAA = true, rgb_fn = true, hsl_fn = true, rgba_fn = true, },
+      ini = { RRGGBBAA = true, },
       css = { css = true, },
       html = { names = false, },
     }
